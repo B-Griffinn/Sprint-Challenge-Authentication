b87a1ad520384a2f8616154e79b4e368ed4134f4
Ben <bigriffin94@outlook.com>
2019-10-18 09:32:28 -0700

    cmon bro, just add.lol

---
+++ auth/auth.spec.js
@@ -0,0 +1,41 @@
+// Imports Needed - supertest + authRouter
+const request = require('supertest');
+const auth = require('./auth-router.js');
+
+describe('POST /register', () => {
+    // sohuld return http status 200
+    it('should return 200 http status code', () => {
+        return request(auth)
+        .post('/register')
+        .then(response => {
+            expect(response.status).toBe(200);
+        })
+        .catch()
+    });
+
+    // should return json
+    test('should return json', async () => {
+        const response = await request(auth).post('/register');
+        // tomatch uses a regular expression the check the value
+        expect(response.type).toMatch(/json/i);
+    })
+});
+
+describe('POST /login', () => {
+    // sohuld return http status 200
+    it('should return 200 http status code', () => {
+        return request(auth)
+        .post('/login')
+        .then(response => {
+            expect(response.status).toBe(200);
+        })
+        .catch()
+    });
+
+    // should return json
+    test('should return json', async () => {
+        const response = await request(auth).post('/login');
+        // tomatch uses a regular expression the check the value
+        expect(response.type).toMatch(/json/i);
+    })
+});
