80d15e2cae55181a644a23b9930439cace9e73d2
Ben <bigriffin94@outlook.com>
2019-10-18 08:37:34 -0700

    Login Endpoint login returns a message of welcome username login returns token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 
 // Required - usersModel + secrets config
 const Users = require('../users/users.model.js');
-// const secrets = require('../config/');
+const secrets = require('../config/secrets.js');
 
 
 router.post('/register', (req, res) => {
@@ -28,15 +28,51 @@ router.post('/register', (req, res) => {
 
 
 
-
-
-
-
-
-
-
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body; // << deconstruct the username & pw from the body
+
+  Users.findBy({ username }) // << look in our DB for the username provided from the body
+    .first() // << the first one to match, unique usernames only so there are no dupes
+    .then(user => {
+      if(user && bcrypt.compareSync(password, user.password)) {
+        // if true, create JWT
+        const token = generateToken(user);
+        // add that token to the response
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "There was a server error when loggin in." })
+    })
 });
+
+
+
+// Generate a token
+function generateToken(user) {
+
+  // 1 Create Payload
+  const payload = {
+    username: user.username,
+    subject: user.id
+  }
+
+  // 2 Create secret - will use dynamic instead for production purposes
+  // const secret = 'I will change to the secrets config file soon....shhh';
+
+  // 3 Create option(s)
+  const options = {
+    expiresIn: '1h', // < adds a claim of the expiration time
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options)
+
+};
 
 module.exports = router;
